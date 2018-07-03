0d0b3130be0f677e44b82bc7d1e30c835f92266d
Vincent Onderwaater <vincentonderwaater@users.noreply.github.com>
2018-07-03 12:16:59 +0200

    Also include tag

--- lib/facebook/messenger/bot.rb
+++ lib/facebook/messenger/bot.rb
@@ -1,6 +1,7 @@
 require 'facebook/messenger/bot/error_parser'
 require 'facebook/messenger/bot/exceptions'
 require 'facebook/messenger/bot/messaging_type'
+require 'facebook/messenger/bot/tag'
 
 module Facebook
   module Messenger
